d003a37c4d5453193e90f58e54be68204815d292
Kelvin Liu <kelvinyliu@gmail.com>
2025-04-09 15:43:57 +0100

    Created main file for starting the GUI

--- Main.java
+++ Main.java
@@ -1,17 +1,35 @@
+import control.BettingController;
+import control.RaceController;
+import model.BettingModel;
+import model.HorseSetupModel;
+import model.Race;
+import model.WeatherCondition;
+import view.BettingView;
+import view.RaceView;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Horse horse1 = new Horse('A', "AAA", 0.9);
-        Horse horse2 = new Horse('B', "BBB", 0.7);
-        Horse horse3 = new Horse('C', "CCC", 0.5);
+        SwingUtilities.invokeLater(() -> {
+            Race raceModel = new Race(20);
+            HorseSetupModel horseModel = new HorseSetupModel();
+            RaceView view = new RaceView();
 
-        Race race = new Race(20);
+            BettingModel bettingModel = new BettingModel();
+            BettingView bettingView = new BettingView();
+            new BettingController(
+                    bettingModel,
+                    bettingView,
+                    horseModel.horses,
+                    raceModel.getWeatherCondition() != null ? raceModel.getWeatherCondition() : WeatherCondition.CLEAR
+            );
 
-        race.addHorse(horse1, 1);
-        race.addHorse(horse2, 2);
-        race.addHorse(horse3, 3);
+            new RaceController(raceModel, view, horseModel);
 
-        race.startRace();
+            view.setVisible(true);
+        });
     }
 }
